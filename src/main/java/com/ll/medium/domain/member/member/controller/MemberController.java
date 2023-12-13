578b5f4dab47363d8be25e09848d30406b764b93
win5948 <win5948@gmail.com>
2023-12-13 15:47:28 +0900

    join

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.ll.medium.domain.member.member.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,4 +18,18 @@ public class MemberController {
     public String showJoin() {
         return "domain/member/member/join";
     }
+
+    @Setter
+    @Getter
+    public static class JoinForm {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+    @PostMapping("/join")
+    public String signup(@Valid JoinForm joinForm) {
+        return "redirect:/";
+    }
 }
+
